b25840c11ebf7d49a6f7d17f53e1971178a3c799
Eder Henrique <ederhscc@gmail.com>
2024-07-03 22:26:43 -0300

    added fech in api

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,27 @@
 import { BsSearch } from "react-icons/bs";
+import { useState } from "react";
 
-const Search = () => {
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>;
+};
+
+const Search = ({ loadUser }: SearchProps) => {
+
+    const [userName, setUserName] = useState("")
+
   return (
     <div>
       <h2>Busque por um usuário:</h2>
       <p>Conheça seus melhores repositórios</p>
       <div>
-        <input type="text" placeholder="Digite o nome do usuário" />
-        <button>
-            <BsSearch />
+        <input
+          type="text"
+          placeholder="Digite o nome do usuário"
+          value={userName || ""}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <button onClick={() => loadUser(userName)}>
+          <BsSearch />
         </button>
       </div>
     </div>
